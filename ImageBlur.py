f27b4b42d6ddfbaaa4813926a59164bd7d2f5602
NoXx897 <145640473+NoXx897@users.noreply.github.com>
2023-09-21 10:39:02 +0530

    Add files via upload

---
+++ ImageBlur.py
@@ -0,0 +1,7 @@
+import cv2
+
+img = cv2.imread("sample_img.jpg")
+blur_image = cv2.GaussianBlur(img, (7,7), 0)
+cv2.imshow('Original Image', img)
+cv2.imshow('Blur Image', blur_image)
+cv2.waitKey(0)
